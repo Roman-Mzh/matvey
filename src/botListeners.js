a781bb4ac033b08213645e9205d39f2ac786580e
Roman Mzh <akentik@gmail.com>
2020-11-02 13:13:29 +0300

    change output order;

--- src/botListeners.js
+++ src/botListeners.js
@@ -37,12 +37,12 @@ motya.onText(/\/poehali\s?([\d\w]*)?\s?(\d*)/, async ({ chat: { id } }, match) =
       .map((p, i) => `${((i + 1) + ' ' + p.name).slice(0,10).padEnd(10)} : ${p.fame} ${p.repairPoints}`);
     motya.sendMessage(
       id,
-      '<pre>Наши герои: \n'
-        + `Актуально на: ${new Intl.DateTimeFormat('ru-RU', { dateStyle: 'short', timeStyle: 'short' }).format(lastUpdate)}\n`
-        + `Идентификатор войны: ${lastWarId}` + '\n'
-        + '-------------------------\n'
-        + formattedParticipants.join('\n')
-        + '</pre>',
+      `Актуально на: ${new Intl.DateTimeFormat('ru-RU', { dateStyle: 'short', timeStyle: 'short' }).format(lastUpdate)}\n`
+      + `Идентификатор войны: ${lastWarId}` + '\n'
+      + '-------------------------\n'
+      + '<pre>Наши герои: \n'
+      + formattedParticipants.join('\n')
+      + '</pre>',
       { parse_mode: 'HTML' }
     );
   } catch (e) {
